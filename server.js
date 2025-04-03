291eb6a321476bddff4df0aa7222fedd162f0032
RakionGamer <mariagabrielanodejs@gmail.com>
2025-04-02 21:24:09 -0400

    Arreglo de CSS

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const express = require('express');
 const port = 3000;
 const app  = require('./controllers/qrController');
 
-
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
